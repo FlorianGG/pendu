37b95f2c8ee5fca1d27b3c32d9ebc59257bf884e
Florian Garcia <florian.prof@gmail.com>
2018-08-04 19:30:25 +0200

    Main feature of the game implemented.
    Still missing the step of the victory

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     letters: this.generateKeyboard(),
     usedLetters: [],
     rightLetters: [],
-    mask: this.maskToDisplay(),
+    mask: [],
   }
 
   //renvoi un mot aléatoire issu de notre liste
@@ -31,46 +31,53 @@ class App extends Component {
   }
 
   handleClick = letter => {
-    const word = this.state.word
+    const { word } = this.state
     const usedLetters = [...this.state.usedLetters]
     const rightLetters = [...this.state.rightLetters]
     usedLetters.push(letter)
     if (word.indexOf(letter) > -1) {
       rightLetters.push(letter)
     }
-    this.setState({ usedLetters, rightLetters })
+    const mask = this.maskToDisplay(word, usedLetters)
+    this.setState({ usedLetters, rightLetters, mask })
   }
 
+  componentWillMount() {
+    const { word, usedLetters } = this.state
+    const mask = this.maskToDisplay(word, usedLetters)
+    this.setState({ mask })
+  }
   // Produit une représentation textuelle de l’état de la partie,
   // chaque lettre non découverte étant représentée par un _underscore_.
-  maskToDisplay() {
-    const mask = []
-    for (let i = 0; i < this.state.word.length; i++) {
-      const wordKey = this.state.word.indexOf(this.state.usedLetters[i]);
-      if ( > -1) {
-        mask.push(this.state.usedLetters[i])
+  maskToDisplay(word, usedLetters) {
+    let mask = []
+    for (let i = 0; i < word.length; i++) {
+      if (usedLetters.indexOf(word[i]) > -1) {
+        mask.push(word[i])
       } else {
         mask.push('_')
       }
     }
-    this.setState({ mask })
+    return mask
   }
 
   render() {
+    const { word, letters, usedLetters, rightLetters, mask } = this.state
+    const won = usedLetters.length === word.lenght
     return (
       <div className="container">
         <div className="maskWord">
-          {this.state.mask.map((value, index) => (
-            <span key="index">{value}</span>
-          ))}
+          {mask.map((letter, index) => <span key={index}>{letter}</span>)}
         </div>
         <div className="keyboard">
-          <Keyboard
-            letters={this.state.letters}
-            onClick={this.handleClick}
-            usedLetters={this.state.usedLetters}
-            rightLetters={this.state.rightLetters}
-          />
+          {!won && (
+            <Keyboard
+              letters={letters}
+              onClick={this.handleClick}
+              usedLetters={usedLetters}
+              rightLetters={rightLetters}
+            />
+          )}
         </div>
       </div>
     )
