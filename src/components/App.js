e63f6a9320fb0d93213e7fb6f1abb149cd660f54
Florian Garcia <florian.prof@gmail.com>
2018-08-04 13:28:09 +0200

    creating onClick feature for keyboard component

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,73 @@ import '../css/App.css'
 
 // Components
 import Keyboard from './Keyboard'
+
+// Charger listes de mots
+import wordList from '../wordList'
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 class App extends Component {
   state = {
+    word: this.getRandomWord(),
     letters: this.generateKeyboard(),
-    indexLettersUsed: [],
-    indexLettersAvailable: [],
+    usedLetters: [],
+    rightLetters: [],
+    mask: this.maskToDisplay(),
+  }
+
+  //renvoi un mot aléatoire issu de notre liste
+  getRandomWord() {
+    const min = 0
+    const max = wordList.length
+    return wordList[Math.floor(Math.random() * (max - min + 1)) + min].split('')
   }
 
   generateKeyboard() {
     return letters.split('')
   }
 
-  handleClick = index => {
-    console.log(index)
+  handleClick = letter => {
+    const word = this.state.word
+    const usedLetters = [...this.state.usedLetters]
+    const rightLetters = [...this.state.rightLetters]
+    usedLetters.push(letter)
+    if (word.indexOf(letter) > -1) {
+      rightLetters.push(letter)
+    }
+    this.setState({ usedLetters, rightLetters })
+  }
+
+  // Produit une représentation textuelle de l’état de la partie,
+  // chaque lettre non découverte étant représentée par un _underscore_.
+  maskToDisplay() {
+    const mask = []
+    for (let i = 0; i < this.state.word.length; i++) {
+      const wordKey = this.state.word.indexOf(this.state.usedLetters[i]);
+      if ( > -1) {
+        mask.push(this.state.usedLetters[i])
+      } else {
+        mask.push('_')
+      }
+    }
+    this.setState({ mask })
   }
 
   render() {
     return (
-      <div className="keyboard">
-        <Keyboard letters={this.state.letters} onClick={this.handleClick} />
+      <div className="container">
+        <div className="maskWord">
+          {this.state.mask.map((value, index) => (
+            <span key="index">{value}</span>
+          ))}
+        </div>
+        <div className="keyboard">
+          <Keyboard
+            letters={this.state.letters}
+            onClick={this.handleClick}
+            usedLetters={this.state.usedLetters}
+            rightLetters={this.state.rightLetters}
+          />
+        </div>
       </div>
     )
   }
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 //CSS
 import '../css/Keyboard.css'
 
-const Keyboard = ({ letters, onClick }) => (
-  <div className="keyboard">
-    {letters.map((letter, index) => (
-      <button
-        key={index}
-        className="buttonKeyboard"
-        onClick={() => onClick(index)}
-      >
-        {letter}
-      </button>
-    ))}
-  </div>
-)
+class Keyboard extends Component {
+  getWrongAttr = letter => {
+    let disabled = ''
+    if (this.props.usedLetters.indexOf(letter) > -1) {
+      disabled = 'disabled'
+    }
+
+    return disabled
+  }
+  getRightClass = letter => {
+    let newClass = 'buttonKeyboard'
+
+    if (this.props.rightLetters.indexOf(letter) > -1) {
+      newClass += ' right'
+    }
+    return newClass
+  }
+
+  render() {
+    return (
+      <div className="keyboard">
+        {this.props.letters.map((letter, index) => (
+          <button
+            disabled={this.getWrongAttr(letter)}
+            key={index}
+            className={this.getRightClass(letter)}
+            onClick={() => this.props.onClick(letter)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
+    )
+  }
+}
 
 export default Keyboard
